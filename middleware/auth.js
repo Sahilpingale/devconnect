a15f41e699a3007dce71de993c4059f7eda9d2b0
Sahil Pingale <sahilpingale@gmail.com>
2022-07-30 13:18:19 +0530

    Initial commit to github

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,21 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-module.exports = function(req,res,next){
-    //Get token from header
-    const token =req.header('x-auth-token')
+module.exports = function (req, res, next) {
+  //Get token from header
+  const token = req.header('x-auth-token');
 
-    //Check if token present
-    if(!token){
-        return res.status(401).json({msg:"No Token, authorization denied"})
-    }
+  //Check if token present
+  if (!token) {
+    return res.status(401).json({ msg: 'No Token, authorization denied' });
+  }
 
-    //Verify token
-    try{
-        const decoded = jwt.verify(token,config.get('jwtSecret'));
-        // console.log(decoded)
-        req.user = decoded.user;
-        next();
-    }catch (err){
-        res.status(401).json({msg:'Token is not valid'})
-    }
-
-}
+  //Verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
